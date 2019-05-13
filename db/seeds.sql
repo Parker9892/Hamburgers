5bb30da576717c2794bf6d8eb35bd4b2959791f2
Hartford2319 <Craig1992jr@gmail.com>
2019-05-12 23:10:05 -0400

    ham

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('The Goat');
+INSERT INTO burgers (burger_name) VALUES ('The Haymaker');
+INSERT INTO burgers (burger_name,devoured) VALUES ('Sylvester', true);
+
